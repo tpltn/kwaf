da63d7e7309cc1f792197044942c34f5bc3ad89c
Timur Platonov <tpltnz@gmail.com>
2018-07-24 09:47:03 +0300

    ok with db migrations

--- src/main/kotlin/org/kwaf/kwaf/entities/Event.kt
+++ src/main/kotlin/org/kwaf/kwaf/entities/Event.kt
@@ -3,12 +3,14 @@ package org.kwaf.kwaf.entities
 import java.util.*
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 
 @Entity
 data class Event(@Id val id: String,
                  val userId: String,
-                 val userAgent: UserAgent,
-                 val url: URL,
+                 @ManyToOne @JoinColumn(name = "userAgent") val userAgent: UserAgent,
+                 @ManyToOne @JoinColumn(name = "url") val url: URL,
                  val method: String,
                  val httpStatus: Int,
                  val committedAt: Date)
