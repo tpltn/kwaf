de6048c2148bbaf39a8760073bb48f44f289a58d
Timur Platonov <tpltnz@gmail.com>
2018-07-27 09:39:43 +0300

    WIP: create event

--- src/main/kotlin/org/kwaf/kwaf/web/controllers/EventsController.kt
+++ src/main/kotlin/org/kwaf/kwaf/web/controllers/EventsController.kt
@@ -4,6 +4,8 @@ import org.kwaf.kwaf.entities.Event
 import org.kwaf.kwaf.gateways.EventGateway
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
@@ -12,5 +14,12 @@ class EventsController(private val eventGateway: EventGateway) {
     @ResponseBody
     fun fetchEvents(): Iterable<Event> {
         return eventGateway.findAll()
+    }
+
+    @PostMapping("/events")
+    @ResponseBody
+    fun createEvent(@RequestBody e: Event): Event {
+        // TODO: prepare input object, than map it to Event
+        return eventGateway.save(e)
     }
 }
